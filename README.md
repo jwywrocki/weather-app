3cd4896e957c18559b25041d185832cdd035df2c
jwywrocki <jaqb1991@gmail.com>
2022-06-05 20:06:56 +0200

    basic app logic

---
+++ README.md
@@ -0,0 +1,18 @@
+![Weather APP](https://github.com/jwywrocki/portfolio/blob/main/img/projects/task_4.png?raw=true)
+
+# Weather APP (Work in progress)
+
+## Introduction
+
+The goal is to create an application that will retrive weather data from API and display it.
+
+## Technologies
+
+-   HTML
+-   CSS
+-   React 18.1.0
+-   Axios 0.27.2
+
+## Live
+
+You can see it live [here](https://jwywrocki.github.io/task_4/)
---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+
+    const api = {
+        key: '1a154a37d24dfd3c1156aee65de4ab61',
+        base: 'http://api.openweathermap.org/data/2.5/',
+    };
+    const url = `${api.base}weather?q=${location}&appid=${api.key}&units=metric`;
+
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data);
+                console.log(response.data);
+            });
+            setLocation('');
+        }
+    };
+
+    return (
+        <div className="App">
+            <div className="search">
+                <input
+                    type="text"
+                    value={location}
+                    onChange={(event) => setLocation(event.target.value)}
+                    onKeyPress={searchLocation}
+                    placeholder="Enter location"
+                    className="search"
+                ></input>
+            </div>
+            <div className="container">
+                <div className="top">
+                    <div className="loc">
+                        <h1>{data.name}</h1>
+                    </div>
+                    <div className="temp">
+                        Temperature:
+                        {data.main ? <p>{data.main.temp}°C</p> : null}
+                    </div>
+                    <div className="desc">
+                        Desc:
+                        {data.weather ? <p>{data.weather[0].main}</p> : null}
+                    </div>
+                </div>
+                <div className="bottom">
+                    <div className="pressure">
+                        Pressure:
+                        {data.main ? <p>{data.main.pressure}hPa</p> : null}
+                    </div>
+                    <div className="humidity">
+                        Humidity:
+                        {data.main ? <p>{data.main.humidity}%</p> : null}
+                    </div>
+                    <div className="wind">
+                        Wind speed:
+                        {data.wind ? <p>{data.wind.speed}KM/H</p> : null}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
